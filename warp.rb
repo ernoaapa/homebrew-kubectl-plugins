756f6dc7a16c51afc0544cc5d9ffdbd95967f898
goreleaserbot <goreleaser@carlosbecker.com>
2018-12-29 12:22:05 +0200

    Brew formula update for kubectl-warp version v0.0.1

--- warp.rb
+++ warp.rb
@@ -3,7 +3,7 @@ class Warp < Formula
   homepage "https://github.com/ernoaapa/kubectl-warp"
   url "https://github.com/ernoaapa/kubectl-warp/releases/download/v0.0.1/kubectl-warp_0.0.1_darwin_amd64.tar.gz"
   version "0.0.1"
-  sha256 "c031070744b21772e9f71fcee7f3515e61671bc90f6684bda909384e92887ada"
+  sha256 "49d9de7dd76d3a11a3e73860be26570b646cccce47af43e448907886b52154c4"
 
   def install
     bin.install "kubectl-warp"
