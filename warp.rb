8628f04660a7a9b523b8ce2074f3328fc0d4ac30
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-11 16:33:05 +0200

    Brew formula update for kubectl-warp version v0.0.2

--- warp.rb
+++ warp.rb
@@ -1,9 +1,9 @@
 class Warp < Formula
   desc "Kubectl plugin to sync files and execute arbitary command in Kubernetes"
   homepage "https://github.com/ernoaapa/kubectl-warp"
-  url "https://github.com/ernoaapa/kubectl-warp/releases/download/v0.0.1/kubectl-warp_0.0.1_darwin_amd64.tar.gz"
-  version "0.0.1"
-  sha256 "49d9de7dd76d3a11a3e73860be26570b646cccce47af43e448907886b52154c4"
+  url "https://github.com/ernoaapa/kubectl-warp/releases/download/v0.0.2/kubectl-warp_0.0.2_darwin_amd64.tar.gz"
+  version "0.0.2"
+  sha256 "9c9821536258b3eb185a44100ad95c7018cb1e0258fb400348843b39f1286136"
 
   def install
     bin.install "kubectl-warp"
